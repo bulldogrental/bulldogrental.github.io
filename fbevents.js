869f5db9d383de36f9ca8cbe7814b5d02690c052
bulldogrental <125169557+bulldogrental@users.noreply.github.com>
2023-02-19 23:40:43 -0800

    Add files via upload

--- fbevents.js
+++ fbevents.js
@@ -17,7 +17,7 @@
 * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-fbq.version="2.9.95";
+fbq.version="2.9.96";
 fbq._releaseSegment = "stable";
 fbq.pendingConfigs=["global_config"];
 fbq.__openBridgeRollout = 1.0;
